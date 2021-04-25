0fa1228bb0bd569703b0eaec2c20d65d1f44ca5c
Jie Fan <13265336+jfandata@users.noreply.github.com>
2021-04-25 09:50:10 -0400

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,11 +11,7 @@ const ContactPage = () => {
             <h2>We'd love to hear from you!</h2>
              
             <ul className={contactStyles.ul}>
-                <li>Say Hi, 你好 or find <strong>Jie F.</strong> on <a href="https://twitter.com/jfstl1" target="_blank"><strong>twitter</strong></a></li>
-                <li>Say Hi or find <strong>Martin C.</strong> on <a href="/contact" target="_blank"><strong>twitter</strong></a></li>
-                <li>Say Hi or find <strong>Maria J.</strong> on <a href="/contact" target="_blank"><strong>twitter</strong></a></li>
-                <li>Say Hi or find <strong>Sohini P.</strong> on <a href="/contact" target="_blank"><strong>twitter</strong></a></li>
-                <li>Say Hi or find <strong>Sunitha R.</strong> on <a href="/contact" target="_blank"><strong>twitter</strong></a></li>
+                <li>Say Hi or find <strong>.</strong> on <a href="https://twitter.com/" target="_blank"><strong>twitter</strong></a></li>
             </ul>
         </Layout>
     )
